c4be83006edbdd70ba473541964d039b388de6f0
pawane <psgqwqu@gmaile.com>
2023-03-10 22:19:08 +0530

    Advanced Javascript

---
+++ script.js
@@ -0,0 +1,46 @@
+// Array of quotes
+const quotes = [
+    {
+      text: "The only way to do great work is to love what you do.",
+      author: "Steve Jobs"
+    },
+    {
+      text: "In the end, we will remember not the words of our enemies, but the silence of our friends.",
+      author: "Martin Luther King Jr."
+    },
+    {
+      text: "Believe you can and you're halfway there.",
+      author: "Theodore Roosevelt"
+    },
+    {
+      text: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+      author: "Winston Churchill"
+    },
+    {
+      text: "I can't change the direction of the wind, but I can adjust my sails to always reach my destination.",
+      author: "Jimmy Dean"
+    },
+    {
+      text: "Happiness is not something ready made. It comes from your own actions.",
+      author: "Dalai Lama"
+    },
+    {
+      text: "You miss 100% of the shots you don't take.",
+      author: "Wayne Gretzky"
+    },
+    {
+      text: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+      author: "Nelson Mandela"
+    },
+    {
+      text: "The best way to predict the future is to invent it.",
+      author: "Alan Kay"
+    },
+    {
+      text: "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
+      author: "Christian D. Larson"
+    }
+  ];
+  
+  // Array of colors
+  
